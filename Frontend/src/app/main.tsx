4279edff49f2b88bc1b4c7e5f169aa2c424d0072
kirill33795 <kirill33795@gmail.com>
2026-05-30 17:52:35 +0300

    конфиг API и переключатель MSW

--- Frontend/src/app/main.tsx
+++ Frontend/src/app/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import { CONFIG } from "@/shared/model/config";
 import { router } from "./router.tsx";
 
 async function bootstrap() {
-  if (import.meta.env.DEV) {
+  if (import.meta.env.DEV && CONFIG.USE_MOCKS) {
     const { enableMocking } = await import("@/shared/api/mocks/index.ts");
     await enableMocking();
   }
