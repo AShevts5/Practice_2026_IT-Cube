8bc0b9621d9c1d4c350c9305960390cdfa7a3451
Anna_Shevtsova9775 <annasevcova01@gmail.com>
2026-06-04 14:54:36 +0300

    Подключение роутер OAuth

--- Backend/src/app/api/router.py
+++ Backend/src/app/api/router.py
@@ -5,6 +5,7 @@ from app.api.v1.admin import invites as admin_invites
 from app.api.v1.admin import teams as admin_teams
 from app.api.v1.auth import admin as auth_admin
 from app.api.v1.auth import captain as auth_captain
+from app.api.v1.auth import oauth as auth_oauth
 from app.api.v1.auth import team as auth_team
 from app.api.v1.captain import profile as captain_profile
 from app.api.v1.public import events as public_events
@@ -17,6 +18,7 @@ api_router.include_router(public_events.router, prefix="/public", tags=["public"
 api_router.include_router(registration.router, prefix="/registration", tags=["registration"])
 api_router.include_router(auth_team.router, prefix="/auth/team", tags=["auth-team"])
 api_router.include_router(auth_captain.router, prefix="/auth/captain", tags=["auth-captain"])
+api_router.include_router(auth_oauth.router, prefix="/auth/oauth", tags=["auth-oauth"])
 api_router.include_router(auth_admin.router, prefix="/auth/admin", tags=["auth-admin"])
 api_router.include_router(captain_profile.router, prefix="/captain", tags=["captain"])
 api_router.include_router(team_cabinet.router, prefix="/team", tags=["team-cabinet"])
